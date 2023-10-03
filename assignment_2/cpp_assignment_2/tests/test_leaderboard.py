3383da5f6d61f69458ef1923b40e4e2a5cf224ae
Dhruv Sangamwar <dsangamwar@gmail.com>
2023-10-02 22:42:46 -0700

    Added gitignore for *.zip

--- assignment_2/cpp_assignment_2/tests/test_leaderboard.py
+++ assignment_2/cpp_assignment_2/tests/test_leaderboard.py
@@ -10,11 +10,6 @@ class TestLeaderboard(unittest.TestCase):
     def setUp(self):
         pass
 
-    # @leaderboard("accuracy")
-    # def test_leaderboard_float(self, set_leaderboard_value=None):
-    #     """Sets a leaderboard value"""
-    #     set_leaderboard_value(round(random.uniform(50, 100), 2))
-
     @leaderboard("Time", "asc")
     def test_another(self, set_leaderboard_value=None):
 
